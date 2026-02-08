3b005f37010ea2984e053c2ba12af9240087fd37
mohamed-foodey <foodeye2022@gmail.com>
2026-02-08 23:40:48 +0300

    added by exercise3

---
+++ exercise3/logical.js
@@ -0,0 +1,14 @@
+let isSunny=true;
+let isWeekend=false;
+
+// and &&
+console.log(isSunny && isWeekend)
+
+// or ||
+console.log(isSunny || isWeekend)
+
+// not !
+console.log(!isSunny )
+
+// not
+console.log(!isWeekend)
