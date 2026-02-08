3c84c6fd499356d9b1b21c99604e1d270cb38b25
mohamed-foodey <foodeye2022@gmail.com>
2026-02-08 22:30:51 +0300

    added exercise2

---
+++ exercise2/app.js
@@ -0,0 +1,28 @@
+let a=15;
+let b="15";
+
+// equal to
+console.log(a == b)
+
+// not equal to
+console.log(a != b)
+
+// strictly equal to
+console.log(a === b)
+
+// not strictly equal to
+console.log(a !== b)
+
+// greater then
+console.log(a>b)
+
+// less then 
+console.log(a < b)
+
+// greater then or equal to
+console.log(a >= b)
+
+// less then or equal to
+console.log(a <= b)
+
+
