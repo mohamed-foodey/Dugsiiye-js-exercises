0fa4a59d1227ca278a2024deeae78dcac575d6cf
mohamed-foodey <foodeye2022@gmail.com>
2026-02-17 17:22:10 +0300

    added by exercise 4

---
+++ exercise4/function.js
@@ -0,0 +1,17 @@
+
+// function add(x,y){
+//     return x+y
+// }
+// console.log(add(10,5))
+// console.log(add(10,10))
+// console.log(add(10,15))
+// console.log(add(10,20))
+// console.log(add(10,30))
+
+const add =function(x,y){
+    console.log(x + y);
+}
+add(85,5)
+add(85,10)
+add(85,15)
+add(95,5)
