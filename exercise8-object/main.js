d9eaad34163f492774b92fff33a094f6280c24f3
mohamed-foodey <foodeye2022@gmail.com>
2026-02-19 14:57:04 +0300

    added exercise8

---
+++ exercise8-object/main.js
@@ -0,0 +1,17 @@
+let car = {
+  make: "Toyota",
+  model: "Corolla",
+  year: 2023
+};
+console.log(car)
+// propert1
+console.log(car.make)
+
+// propert2
+console.log(car.model)
+
+// propert3
+console.log(car.year)
+
+
+
