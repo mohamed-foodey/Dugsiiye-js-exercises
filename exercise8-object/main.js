5fa0e746ede8d5eea624c6c40ccb79c28e1e7371
mohamed-foodey <foodeye2022@gmail.com>
2026-02-19 15:24:55 +0300

    added exercise9

--- exercise8-object/main.js
+++ exercise8-object/main.js
@@ -1,9 +1,13 @@
 let car = {
   make: "Toyota",
   model: "Corolla",
-  year: 2023
+  year: 2023,
+
+  start :function(){
+        console.log("the has start " + this.make)
+    }
 };
-console.log(car)
+console.log(car.start())
 // propert1
 console.log(car.make)
 
