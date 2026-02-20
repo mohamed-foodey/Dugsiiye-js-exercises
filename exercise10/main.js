7a73c5dc2b741989dd8e5907ca31533e6e42dc96
mohamed-foodey <foodeye2022@gmail.com>
2026-02-20 22:51:37 +0300

    added exercise10

---
+++ exercise10/main.js
@@ -0,0 +1,26 @@
+// arrays and objects
+
+let libarary = [
+    {
+        tittle: "Things Fall Apart",
+        author: "Chinua Achebe",
+        year: 1958
+    },
+
+    {
+        tittle: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        year: 1960
+    },
+
+    {
+        tittle: "The Alchemist",
+        author: "Paulo Coelho",
+        year: 1988
+    }
+]
+
+console.log(libarary)
+console.log(libarary[0].tittle)
+
+console.log(libarary[1].author)
