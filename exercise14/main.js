a5522ce7af808d491c846429608cc6a32b9fbf63
mohamed-foodey <foodeye2022@gmail.com>
2026-02-22 15:37:18 +0300

    added exercise13 do while loop

---
+++ exercise14/main.js
@@ -0,0 +1,7 @@
+// do while loop example
+let number;
+do {
+    number = prompt("Fadlan gali number ka weyn 10:");
+    console.log("Waxaad gashay: " );
+} while (number <= 10); 
+alert("Waad galisay number ka weyn 10: " );
