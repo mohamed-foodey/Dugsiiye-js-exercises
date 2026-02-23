8da9f9a8e432f5295e29d04764e99d431ef93247
mohamed-foodey <foodeye2022@gmail.com>
2026-02-23 07:31:53 +0300

    added exercise16 if and eelse statements

---
+++ exercise16/if-else.js
@@ -0,0 +1,10 @@
+let score=50;
+score=prompt("Enter your score:");
+if(score>=50){
+    console.log("You passed the exam");
+    alert("Congratulations! You passed the exam.");
+}
+else{
+    console.log("You failed the exam");
+    alert("Sorry, you failed the exam. Better luck next time.");
+}
