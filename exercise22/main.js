889742933e554bfa62c1fb5597ec696f0c44b2cb
mohamed-foodey <foodeye2022@gmail.com>
2026-03-07 21:21:16 +0300

    added exercises22

---
+++ exercise22/main.js
@@ -0,0 +1,5 @@
+// destructuring assignment
+let colors = ['red', 'green', 'blue'];
+let [firstColor, secondColor, thirdColor] = colors;
+console.log(firstColor); // Output: red
+console.log(thirdColor);
