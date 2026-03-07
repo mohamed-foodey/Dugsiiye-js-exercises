5c4d46c4935466f9b4abf5d038986ac698179c3a
mohamed-foodey <foodeye2022@gmail.com>
2026-03-07 21:35:46 +0300

    added exercise23

---
+++ exercise23/main.js
@@ -0,0 +1,5 @@
+// object destructuring assignment
+let cars ={make: "Toyota", model: "Corolla", year: 2020};
+let {make, model} = cars;
+console.log(make); // Output: Toyota
+console.log(model); // Output: Corolla
