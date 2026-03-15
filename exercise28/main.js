ee7da26a2ff860e1265c6361551bcf67953f7b23
mohamed-foodey <foodeye2022@gmail.com>
2026-03-15 20:36:57 +0300

    added exercise28 async/await

---
+++ exercise28/main.js
@@ -0,0 +1,30 @@
+// async and await assignment
+function fetchDataProducts(){
+    return new Promise ((resolve, reject) => {
+
+        setTimeout(() => {
+            let success = true;
+            if (success){
+                resolve({name:"laptop",price:1000,brand:"dell",model:"xps 13"});
+            }
+            else{
+                reject("error fetching product data");
+            }
+        },2000)
+    })
+}
+// fetchDataProducts()
+// .then((data) => console.log("product data: ",data))
+// .catch((error) => console.error(error));
+async function getProductData(){
+    // success case
+    try{
+        const data = await fetchDataProducts();
+        console.log("product data: ",data);
+    }
+    // error case
+    catch(error){
+        console.error(error);
+    }
+}
+getProductData();
