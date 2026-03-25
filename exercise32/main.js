95c539f4f9d6fc4184a8513f1c5fff714156ea4a
mohamed-foodey <foodeye2022@gmail.com>
2026-03-25 07:14:57 +0300

    added exercise32 dom

---
+++ exercise32/main.js
@@ -0,0 +1,12 @@
+// dom assignment
+
+
+const header = document.querySelector('h1');
+console.log(header);
+
+
+const hobbiesList = document.querySelector('ul');
+console.log(hobbiesList);
+
+const hobbiesItems = document.querySelectorAll('li');
+console.log(hobbiesItems);
