d41c791e78f7aa23db2c68c1feae5d47e4ad9222
mohamed-foodey <foodeye2022@gmail.com>
2026-03-25 07:52:11 +0300

    added exercise33 manipulating dom

---
+++ exercise33/main.js
@@ -0,0 +1,9 @@
+// dom manipulating
+function addContent(){
+    header.textContent = "hi everyone welcome to manipulating dom elements"
+}
+const text = document.querySelector('.text')
+
+function changeContent(){
+    text.innerHTML = "hi welcome to my website <strong> dom manipulation </strong> ";
+}
